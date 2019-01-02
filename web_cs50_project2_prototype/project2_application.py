031789b7c6cb19bea62faaff38bb7de974cdb259
lKilgoretrout <30457387+lKilgoretrout@users.noreply.github.com>
2019-01-02 11:40:35 -0800

    *non-functional* working on message deletion

--- web_cs50_project2_prototype/project2_application.py
+++ web_cs50_project2_prototype/project2_application.py
@@ -1,5 +1,4 @@
-import os, time
-
+import os, datetime
 from flask import Flask, render_template, url_for, request, session, redirect, jsonify
 from flask_socketio import SocketIO, emit, join_room, leave_room
 from flask_session import Session
@@ -13,6 +12,24 @@ Session(app)
 
 display_names_list = []
 channel_list = ["General"]
+time = datetime.datetime.now()
+now = time
+
+rooms = {}
+
+startup_message = {
+    "channel": "General",
+    "user_from": "Flack",
+    "user_to": "",
+    "timestamp": now.strftime("%a %b %d %I:%M:%S"), 
+    "msg_txt": "Welcome to my WebCS50 Project2: 'Flack' ",
+    "deleteID": None }
+
+channel_messages = {
+    "General": {
+        'messages': [startup_message]
+}}
+
 
 
 @app.route('/')
@@ -21,7 +38,7 @@ def index():
         user_id = session['user_id']
         return render_template("project2_index.html", message=f"Welcome to my WebCS50 Project2, {user_id}", user_id=user_id)
     else:
-        return render_template("project2_register.html", message=f"Please enter a user_id", user_id=None)
+        return render_template("project2_register.html", message=f"Please enter a user_id (1-32 characters)", user_id=None)
         
         
 @app.route('/logout')
@@ -41,11 +58,72 @@ def register():
         return render_template('project2_register.html', \
             message=f"Please enter a user_id", user_id=None)
 
-@app.route("/get_channels", methods=["POST"])
-def get_channels():
-    return jsonify({"success": True, "channel_list": channel_list})
+@app.route("/get_messages", methods=["POST"])
+def get_messages():
+    ''' within 'channel_messages' list of messages found at :
+        channel_messages[channel]['messages']'''
+    channel = request.form.get("channel")
+    display_name = request.form.get("displayName")
 
-###########
+    #print('@app.route("/get_messages", methods=["POST"])')
+    #print('channel = ', channel)
+    
+    # if no messages, set to startup_message
+    try:
+        message_list = channel_messages[channel]['messages']
+        return jsonify({"success": True, "messages": message_list})
+
+    except KeyError:
+        message_list = channel_messages['General']['messages']
+        return jsonify({"success": False, "messages": message_list})
+    
+              
+  
+@socketio.on("submit message")
+def new_message(data):
+    '''
+     data = socket.emit('submit message', {'msg_txt': msg_txt, 'channel': channel,'timestamp': current_time, 'user_from': displayName});
+    '''
+    channel = data["channel"]
+    user_from = data["user_from"]
+    msg_txt = data["msg_txt"]
+    timestamp = data["timestamp"]
+    deleteID = data["deleteID"]
+
+    message = {"msg_txt": msg_txt,
+               "channel": channel,
+               "timestamp": timestamp,
+               "user_from": user_from,
+               "deleteID": deleteID }
+
+    ''' channel_messages = {
+            "General": {
+                'messages': [startup_message]
+            }
+            "channel": {
+                'messages': [] 
+            }
+    }
+    '''
+    # create 'channel' key in channel_messages
+    if channel not in channel_messages:
+        channel_messages[channel] = {'messages': [] }
+    
+    # if the number of messages in a channel exceeds 100, delete first message
+    if len(channel_messages[channel]['messages']) >= 100:
+        del channel_messages[channel]['messages'][0]
+
+    channel_messages[channel]['messages'].append(message)
+
+##################
+    #print('within @socketio.on("submit message")')
+    #print('[channel] = ', channel)
+    #print("channel_messages[channel]['messages']", channel_messages[channel]['messages'])
+
+    emit("create message", message, broadcast=True)
+        
+    return jsonify({"success": True})
+    
 
 @socketio.on('submit channel')
 def create_channel(channel):
@@ -64,11 +142,19 @@ def join(data):
     
     return jsonify({"success": True})
 
+@socketio.on('delete channel')
+def delete_message(deleteID, channel):
+    for msg in channel_messages[channel]['messages']:
+        if msg['deleteID'] == deleteID:
+            del channel_messages[channel]['messages'][msg]
 
-
-
-
-
+'''    
+    channel_messages = {
+    "General": {
+        'messages': [startup_message]
+    }}
+'''
+    
 
 
 if __name__ == "__main__":
--- web_cs50_project2_prototype/static/project2_index.js
+++ web_cs50_project2_prototype/static/project2_index.js
@@ -2,33 +2,85 @@ var channel_list = [];
 var current_channel;
 
 function clickHandler(channel_name) {
+    /** function runs from ClickHandlerObject stored in each
+     * channel's table_data (inside add_channel())
+     * Sets current channel to bold and configure's messages
+     */
 
-    let current_channel =  channel_name;
+    current_channel = channel_name;
     
+    //let current_channel = localStorage.getItem('current_channel');
+
     // change fontWeight of all channels to normal: 
     document.querySelectorAll('.channel').forEach(td => {
         td.style.fontWeight = "normal";
     });
 
-    //msgType = "PUBLIC";
     let channel_header = document.getElementById("channel_header");
 
     // current_channel <fontWeight> to bold
     document.getElementById(current_channel).style.fontWeight = "bold";
 
     channel_header.innerHTML = " Messages on " + current_channel + " channel";
-    localStorage.setItem("channel", current_channel);
-    //configure_msgs(global_current_channel, msgType);
+    localStorage.setItem("current_channel", current_channel);
+    configure_messages(current_channel);
 }
 
+// Clear messages when switching channels
+function clear_messages() {
+    var messageList = document.getElementById('message_list');
+
+    while (messageList.firstChild) {
+	messageList.removeChild(messageList.firstChild);
+    }
+}
+
+function create_message(message) {
+/** message = {"msg_txt": msg_txt,
+               "channel": channel,
+               "timestamp": timestamp,
+               "user_from": user_from
+               "deleteID": deleteID} 
+*/
+    var channel = message['channel']
+    var deleteID = message['deleteID']
+    var table_row = document.createElement('tr');
+    var table_data = table_row.insertCell(-1);
+    table_data.setAttribute("id", message["user_from"])
+    table_data.setAttribute("data-deleteID", deleteID);
+
+    let timeStamp = String("<font class='timeStamp'>" + message["timestamp"] + "</font>");
+    let displayName = String("<font class='displayName'> @" + message["user_from"] + "</font><br>");
+    let messageText = String(message["msg_txt"]);
+
+    let newMessage = timeStamp + ' ' + displayName + ' ' + messageText;
+    table_data.innerHTML = newMessage
+
+    // event handler for deleting messages:
+    // clicking on a message runs deleteMessage()
+    var deleteMessageHandler = table_row.querySelector("td");
+    deleteMessageHandler.addEventListener("click", function () {deleteMessage(deleteID, channel);});
+    
+    document.querySelector('#message_list').append(table_row);
+}
+
+function deleteMessage(deleteID, channel) {
+    //var messages = document.querySelector("#message_list");
+    //var match = messages.querySelector("tr[data-deleteID= deleteID]");
+
+    if (confirm("Do you want to delete this message forever and ever ?")) {
+        match.pop();
+        emit("delete message", deleteID, channel);
+      } else {
+        alert("Whatever... I wasn't going to delete that message anyway");
+      } 
+    }
 
 function add_channel(channel_name, select) {
 /** adds channel as table_row with table_data into (id=channels) table 
 *   if select = 1 : uses clickHandler to change font and message_header in room (channel)  
 */
-
     var table_row = document.createElement('tr');
-    
     var table_data = table_row.insertCell(-1);
     table_data.innerHTML = channel_name;
     table_data.setAttribute("id", channel_name);
@@ -53,45 +105,48 @@ function add_channel(channel_name, select) {
     }
 };
 
-/*
-function get_channels() {
-    // Get List of Channels
+function configure_messages(channel) {
+/** runs inside clickhandler, gets messages for respective channels 
+*/
+    // Clear out old message list
+    clear_messages();
+
+    // Get Messages on this channel
     const request = new XMLHttpRequest();
-    request.open('POST', '/get_channels');
-	 
-    if (localStorage.getItem('current_channel')) {
-	current_channel = localStorage.getItem('channel');
-    }
-    else {
-    current_channel = "General";
-    }
-    // callback function 
+    request.open('POST', '/get_messages');
+
+    // Callback function for when request completes
     request.onload = () => {
 
-	// Extract JSON data from request
-	const response = JSON.parse(request.responseText);
-		
-	// unpack list of channels 
-	if (response.true) {
-	    var channels = data["channel_list"];
-	    for (var i = 0, len = channels.length; i < len; i++) {
-		if (channels[i] == current_channel){
-		    add_channel(channels[i], 1);
-		}
-		else {
-		    add_channel(channels[i],0);
-		}
+	const data = JSON.parse(request.responseText);
+	
+    // Extract dictionary of messages and populate message pane
+	if (data.success) {
+	    console.log("configure_messages: success.")
+	    var messages = data["messages"];
+	    for (var i = 0, len = messages.length; i < len; i++) {
+		create_message(messages[i]);
 	    }
-	}
-	else {
-	    console.log("function get_channels() did not work");
-	}
     }
-    request.send();
-}
-*/
+    else {
+        console.log('No messages in get_Messages, using startup_message');
+        
+        create_message(data["messages"][0]);
+    }
+    }
 
-// create a channel 
+    // Add data to send with request for messages on this channel
+    const data = new FormData();
+    //let current_channel = localStorage.getItem('current_channel');
+    data.append('channel', channel);
+    data.append('displayName', displayName);
+
+    // Send request
+    request.send(data);
+    return false;
+}
+
+
 document.addEventListener('DOMContentLoaded', () => {
     
     // Connect to websocket
@@ -107,49 +162,101 @@ document.addEventListener('DOMContentLoaded', () => {
 
     socket.on('connect', () => {
 	    var id = socket.io.engine.id;
+        var displayName = localStorage.getItem('dn')
 	    socket.emit("join", {"displayName": displayName, "room": id});
-	    //get_channels();
-	    //configure_users();
 	});
 
-
-
-    // the channel submit button is disabled by default:
+    // the channel/message submit button is disabled by default:
     document.querySelector('#channel_submit').disabled = true;
+    document.querySelector('#submit_message').disabled = true;
 
-     // Enable button only if there is text in the input field
+     // Enable channel button only if there is text in the input field
     document.querySelector('#new_channel').onkeyup = () => {
-        if (document.querySelector('#channel_name').value.length > 0) 
+        if (document.querySelector('#channel_name').value.length > 0) {
             document.querySelector('#channel_submit').disabled = false;
-            
-        else
+
+        } else {
             document.querySelector('#channel_submit').disabled = true;
-            
+
+        }
         // if the channel name is a duplicate, don't allow submission:
         if (channel_list.length > 0)  
             if (channel_list.includes(document.querySelector('#channel_name').value))    
                 document.querySelector('#channel_submit').disabled = true;
-    };
 
-// channel submit: 
+        // limit channel names to < 32 characters
+        if (document.querySelector('#channel_name').value.length > 32) {
+            document.querySelector('#channel_submit').disabled = true;
+            alert("Channel names must be fewer than 32 characters");
+        }
+    };
+    
+    // Enable message button only if (0 < message_text < 65)
+    document.querySelector('#new_message').onkeyup = () => {
+        if (document.querySelector('#message_text').value.length < 1)
+            document.querySelector('#submit_message').disabled = false;
+            
+        if (document.querySelector('#message_text').value.length > 64) {
+            document.querySelector('#submit_message').disabled = false;
+            alert("Messages must be fewer than 64 characters")
+            
+
+        } else {
+            document.querySelector('#submit_message').disabled = false;
+        };
+    };
+    
+
+    // channel submit form handler: 
 
     document.querySelector('#new_channel').onsubmit = () => {
         var channel = document.querySelector('#channel_name').value;
         // Clear input field
         document.querySelector('#channel_name').value = '';
-        
+        localStorage.setItem('current_channel', channel)
+        current_channel = channel;
         socket.emit('submit channel', channel);
-        //console.log("submit channel emitted successfully")
         return false;
     };
         
     socket.on('create channel', channel_name => {
-        //console.log("socket.on create channel ran")
         add_channel(channel_name, 1);
-        
     })
-        
     
+
+    // message submit form handler
+
+    document.getElementById("new_message").onsubmit = () => {
+	    
+	var msg_txt  = document.getElementById('message_text').value;
+	var current_time = new Date().toLocaleTimeString();
+	var displayName = document.getElementById('displayName').innerHTML;
+    var channel = localStorage.getItem('current_channel')
+    var deleteID = Math.floor(new Date().getTime());
+
+	document.getElementById('message_text').value = "";
+	//document.getElementById('submit_message').disabled = true;
+	
+	// send message form data to server 
+    socket.emit('submit message', 
+        {'msg_txt': msg_txt,
+         'channel': channel,
+         'timestamp': current_time,
+         'user_from': displayName,
+         'deleteID': deleteID});
+
+    //console.log('socket.emit ("submit message") done sent!')
+	return false;
+    };
+    
+    
+    // When a new message is announced, add to the message list
+    socket.on('create message', message => {
+	    //if (data["channel"] == current_channel) {
+		create_message(message);
+        //console.log("socket.on('create message') received response")
+	    //}
+	});
 });
 
 
